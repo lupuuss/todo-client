15a80fee2a6685b72deea56b6d7b647f4a358df9
Damian Baczyński <224250@edu.p.lodz.pl>
2021-02-10 15:46:44 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,86 @@
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
+
+plugins {
+    kotlin("multiplatform") version "1.4.30"
+    id("com.android.application")
+}
+
+group = "com.github.lupuuss.todo"
+version = "1.0"
+
+repositories {
+    google()
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://dl.bintray.com/kotlin/kotlin-js-wrappers") }
+}
+
+kotlin {
+    android()
+    js(LEGACY) {
+        browser {
+            binaries.executable()
+            webpackTask {
+                cssSupport.enabled = true
+            }
+            runTask {
+                cssSupport.enabled = true
+            }
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                    webpackConfig.cssSupport.enabled = true
+                }
+            }
+        }
+    }
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("com.google.android.material:material:1.3.0")
+            }
+        }
+        val androidTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+                implementation("junit:junit:4.13.1")
+            }
+        }
+        val jsMain by getting {
+            dependencies {
+                implementation("org.jetbrains:kotlin-react:16.13.1-pre.113-kotlin-1.4.0")
+                implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.113-kotlin-1.4.0")
+                implementation("org.jetbrains:kotlin-styled:1.0.0-pre.113-kotlin-1.4.0")
+                implementation("org.jetbrains:kotlin-react-router-dom:5.1.2-pre.113-kotlin-1.4.0")
+                implementation("org.jetbrains:kotlin-redux:4.0.0-pre.113-kotlin-1.4.0")
+                implementation("org.jetbrains:kotlin-react-redux:5.0.7-pre.113-kotlin-1.4.0")
+            }
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+    }
+}
+
+android {
+    compileSdkVersion(30)
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        applicationId = "com.github.lupuuss.todo.mainModule"
+        minSdkVersion(24)
+        targetSdkVersion(30)
+    }
+}
+
+tasks.getByName<KotlinWebpack>("jsBrowserProductionWebpack") {
+    outputFileName = "output.js"
+}
