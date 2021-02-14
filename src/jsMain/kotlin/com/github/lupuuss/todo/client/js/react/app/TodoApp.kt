160de85e7e87cc04298269a18d42011f1656eea5
lupuuss <damian.baczynski@gmail.com>
2021-02-14 19:59:23 +0100

    Removes testing repository call from TodoApp

--- src/jsMain/kotlin/com/github/lupuuss/todo/client/js/react/app/TodoApp.kt
+++ src/jsMain/kotlin/com/github/lupuuss/todo/client/js/react/app/TodoApp.kt
@@ -1,34 +1,13 @@
 package com.github.lupuuss.todo.client.js.react.app
 
-import com.github.lupuuss.todo.client.core.TodoKodein
-import com.github.lupuuss.todo.client.core.repository.TaskRepository
 import com.github.lupuuss.todo.client.js.react.Colors
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
 import kotlinx.css.*
-import org.kodein.di.direct
-import org.kodein.di.instance
 import react.RBuilder
 import react.RComponent
 import styled.css
 import styled.styledDiv
 
 class TodoApp : RComponent<dynamic, dynamic>() {
-
-    override fun componentWillMount() {
-        val repo = TodoKodein.di.direct.instance<TaskRepository>()
-
-        GlobalScope.launch {
-            async {
-                try {
-                    println(repo.getTodoTasks(0, 10))
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
-            }
-        }
-    }
 
     override fun RBuilder.render() {
 
