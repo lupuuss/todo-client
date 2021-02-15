06131ab39439e11213553f71c3f491a5b46c5837
lupuuss <damian.baczynski@gmail.com>
2021-02-15 15:40:19 +0100

    Adds routing

--- src/jsMain/kotlin/com/github/lupuuss/todo/client/js/react/app/TodoApp.kt
+++ src/jsMain/kotlin/com/github/lupuuss/todo/client/js/react/app/TodoApp.kt
@@ -1,36 +1,59 @@
 package com.github.lupuuss.todo.client.js.react.app
 
+import com.github.lupuuss.todo.client.core.TodoKodein
+import com.github.lupuuss.todo.client.core.auth.AuthManager
 import com.github.lupuuss.todo.client.js.react.Colors
 import com.github.lupuuss.todo.client.js.react.globalStyle
+import com.github.lupuuss.todo.client.js.react.home.home
 import com.github.lupuuss.todo.client.js.react.login.login
 import kotlinx.css.*
+import org.kodein.di.instance
 import react.RBuilder
 import react.RComponent
+import react.router.dom.*
 import styled.css
 import styled.styledDiv
 
-class TodoApp : RComponent<dynamic, dynamic>() {
+class TodoApp(props: dynamic) : RComponent<dynamic, dynamic>(props) {
 
-    private fun routeToHome() {
-        println("LOGGED IN :D")
+    private val di = TodoKodein.di
+
+    private val authManager: AuthManager by di.instance()
+
+    private fun onLoggedIn(history: RouteResultHistory) {
+        history.push("/")
     }
 
     override fun RBuilder.render() {
-
         globalStyle()
 
-        topBar()
+        browserRouter {
 
-        styledDiv {
+            topBar()
 
-            css {
-                width = LinearDimension.auto
-                height = 100.pct
-                backgroundColor = Colors.secondary
-            }
+            styledDiv {
 
-            login {
-                onSuccess = this@TodoApp::routeToHome
+                css {
+                    width = LinearDimension.auto
+                    height = 100.pct
+                    backgroundColor = Colors.secondary
+                }
+
+                switch {
+
+                    route("/", exact = true) {
+                        if (authManager.isUserLoggedIn()) home() else redirect(to = "/login")
+                    }
+
+                    route("/login") { props: RouteResultProps<dynamic> ->
+
+                        login {
+                            onSuccess = {
+                                onLoggedIn(props.history)
+                            }
+                        }
+                    }
+                }
             }
         }
     }
