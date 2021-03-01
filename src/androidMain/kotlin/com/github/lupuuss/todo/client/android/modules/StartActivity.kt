f5e85fbfae004f78b5543b8789a798570a234a0f
Damian Baczyński <224250@edu.p.lodz.pl>
2021-03-01 01:04:23 +0100

    Now StartActivity.kt uses di() function

--- src/androidMain/kotlin/com/github/lupuuss/todo/client/android/modules/StartActivity.kt
+++ src/androidMain/kotlin/com/github/lupuuss/todo/client/android/modules/StartActivity.kt
@@ -5,21 +5,24 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.github.lupuuss.todo.client.android.R
 import com.github.lupuuss.todo.client.android.modules.login.LoginActivity
-import com.github.lupuuss.todo.client.core.TodoKodein
 import com.github.lupuuss.todo.client.core.auth.AuthManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import org.kodein.di.DIAware
+import org.kodein.di.android.di
 import org.kodein.di.instance
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
 import kotlin.time.seconds
 
 
-class StartActivity : AppCompatActivity(), CoroutineScope by MainScope() {
+class StartActivity : AppCompatActivity(), DIAware, CoroutineScope by MainScope() {
 
-    private val authManager: AuthManager by TodoKodein.di.instance()
+    override val di by di()
+
+    private val authManager: AuthManager by instance()
 
     @ExperimentalTime
     override fun onCreate(savedInstanceState: Bundle?) {
